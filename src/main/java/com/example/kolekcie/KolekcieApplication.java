bc51ef990d518562a63366f7204c80c94a0bff6e
Аня <grigorevna.antoninka@mail.ru>
2023-04-03 19:21:30 +0700

    Initial commit

---
+++ src/main/java/com/example/kolekcie/KolekcieApplication.java
@@ -0,0 +1,13 @@
+package com.example.kolekcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolekcieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolekcieApplication.class, args);
+    }
+
+}
